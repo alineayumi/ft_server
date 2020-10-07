018dd72ca1476d8163c6bc84f87778c77204f517
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2020-10-07 13:15:39 -0300

    feat: added ssl key

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@
 #    By: afukuhar <afukuhar@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/24 14:59:24 by afukuhar          #+#    #+#              #
-#    Updated: 2020/10/04 18:36:18 by afukuhar         ###   ########.fr        #
+#    Updated: 2020/10/04 22:18:36 by afukuhar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,10 +15,10 @@
 
 FROM debian:buster
 
+RUN apt-get update && apt-get install -y apt-utils vim wget openssl
+
 RUN apt-get update && apt-get install -y \
-nginx \
-mariadb-server \
-php-fpm php-mysql 
+nginx 
 
 EXPOSE 80 443
 
--- srcs/run_ftserver.sh
+++ srcs/run_ftserver.sh
@@ -9,6 +9,20 @@ chmod -R 755 /var/www/*
 # Create server folder
 mkdir /var/www/localhost
 
+echo " ----------------- "
+echo "| SETTING SSL KEY |"
+echo " ----------------- "
+
+mkdir /etc/nginx/ssl
+openssl req -newkey rsa:4096 \
+            -x509 \
+            -sha256 \
+            -days 3650 \
+            -nodes \
+            -out /etc/nginx/ssl/localhost.pem \
+            -keyout /etc/nginx/ssl/localhost-key.pem \
+			-subj "/C=US/ST=California/L=Fremont/O=42/OU=aline/CN=ft_server"
+
 echo " --------------- "
 echo "| SETTING NGINX |"
 echo " --------------- "
@@ -29,7 +43,7 @@ echo ""
 
 echo "[ LINKS & COMMANDS ]"
 echo "docker build -t ft_server ."
-echo "docker run --name ft_server -d -p 8080:80 -p ft_server"
+echo "docker run --name ft_server -d -p 80:80 -p 443:443 ft_server"
 echo "https://localhost"
 
 bash
